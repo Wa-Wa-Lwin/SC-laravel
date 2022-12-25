a2913b7fd536747f08aeb6750a27b448aaaea63b
Wa Wa Lwin <wlu4699@gmail.com>
2022-12-25 23:09:57 +0630

    in process

--- routes/web.php
+++ routes/web.php
@@ -10,20 +10,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/greet/{one}/{two}', [TestController::class, 'greet']);
-
-// Route::get('/greeting', [TestController::class, 'greeting']);
-// Route::get('/about', [TestController::class, 'about']);
-// Route::get('/posts', [PostController::class, 'post']);
-// Route::get('/posts/create', [PostController::class, 'create']);
-// Route::post('/posts', [PostController::class, 'store']);
-// Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
-// Route::post('/posts/{id}', [PostController::class, 'update']);
-// Route::post('/posts/{id}/delete', [PostController::class, 'destory']);
-//Route::get('/posts/create/{title}', [PostController::class, 'create']);
-// ('/route', [array])
-// [Controller, Method]
-
 Route::resource('/posts', PostController::class);
 
 Route::resource('/categories', CategoryController::class);
